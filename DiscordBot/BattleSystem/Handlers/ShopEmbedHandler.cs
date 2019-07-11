a40be234fad43934b78daaffb2bad2476fa5832d
MuninCoding <46007118+MuninCoding@users.noreply.github.com>
2019-07-12 00:00:30 +0200

    Started shop module integration

---
+++ DiscordBot/BattleSystem/Handlers/ShopEmbedHandler.cs
@@ -0,0 +1,57 @@
+﻿using Discord;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace DiscordBot.BattleSystem.Handlers
+{
+    public class ShopEmbedHandler
+    {
+        internal static Embed ShopEmbed()
+        {
+            var shopsEmbed = new EmbedBuilder();
+
+           shopsEmbed.WithColor(Color.DarkRed)
+                     .WithTitle("Shops")
+                     .WithDescription("Below is a list with all currently available Shop´s")
+                     .AddField("Potions", "?help - Shows bot information with all available commands")
+                     .AddField("Weapons", "?sharerank {user} - Shares your rank with the specified user")
+                     .AddField("Armor", "?clear - Clear the chat in the Channel there you write the Message")
+                     .AddField("Shields", "?mute<@playername><MuteState> - Mute player for Voicechannel (MuteState = True|False)")
+                     .AddField("Points", "?kick<@playername><Reason> - Kick a player from Server with a specified reason")
+                     .WithFooter(footer => footer.Text = "©RavenplaysGuardianBot")
+                     .WithCurrentTimestamp();
+
+            var shopsInfoEmbed = shopsEmbed.Build();
+            return shopsInfoEmbed;
+        }
+
+        internal static Embed PotionsEmbed()
+        {
+            throw new NotImplementedException();
+        }
+
+        internal static Embed WeaponEmbed()
+        {
+            throw new NotImplementedException();
+        }
+
+        internal static Embed ArmorEmbed()
+        {
+            throw new NotImplementedException();
+        }
+
+        internal static Embed ShieldEmbed()
+        {
+            throw new NotImplementedException();
+        }
+
+        internal static Embed BattlePointsEmbed()
+        {
+            throw new NotImplementedException();
+        }
+
+    }
+}
---
+++ DiscordBot/Modules/BattleModules/ShopModule.cs
@@ -0,0 +1,47 @@
+﻿using Discord;
+using Discord.Commands;
+using DiscordBot.BattleSystem.Handlers;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace DiscordBot.Modules.BattleModules
+{
+    public class ShopModule : ModuleBase<SocketCommandContext>
+    {
+        [Command("shop")]
+        public async Task Shop (string type = null)
+        {
+            if (type != null)
+            {
+                switch(type)
+                {
+                    case "potion":
+                        await ReplyAsync(embed: ShopEmbedHandler.PotionsEmbed());
+                        break;
+                    case "weapon":
+                        await ReplyAsync(embed: ShopEmbedHandler.WeaponEmbed());
+                        break;
+                    case "armor":
+                        await ReplyAsync(embed: ShopEmbedHandler.ArmorEmbed());
+                        break;
+                    case "shield":
+                        await ReplyAsync(embed: ShopEmbedHandler.ShieldEmbed());
+                        break;
+                    case "battlepoints":
+                        await ReplyAsync(embed: ShopEmbedHandler.BattlePointsEmbed());
+                        break;
+                    default:
+                        break;
+                }
+            }
+            else
+            {
+                //send shopsInfoEmbed
+                await ReplyAsync(embed: ShopEmbedHandler.ShopEmbed());
+            }
+        }
+    }
+}
